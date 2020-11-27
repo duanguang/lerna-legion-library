9875aed560c03505fc9e43b9c788721d123d57f1
duanguang <duanguang@hoolinks.com>
2020-11-27 16:47:50 +0800

    chore(release): 更新临时legions-micro-servicev 0.0.5测试版本

---
+++ packages/legions-micro-service/src/sandbox/patchers/index.ts
@@ -0,0 +1,55 @@
+
+
+import { Freer } from '../../interfaces';
+import { patchLooseSandbox, patchStrictSandbox } from './dynamicAppend/';
+import hijackHistoryListener from '../../hijackers/historyListener';
+import hijackTimer from '../../hijackers/timer';
+import hijackWindowListener from '../../hijackers/windowListener';
+/* import hijackDynamicAppend from './dynamicHeadAppend'; */
+import ProxySandbox from '../proxySandbox';
+import { SandBoxType } from '../../utils';
+export function patchAtMounting(
+  appName: string,
+  elementGetter: () => HTMLElement | ShadowRoot,
+  sandbox: ProxySandbox,
+  scopedCSS: boolean,
+  excludeAssetFilter?: Function,
+): Freer[] {
+  const basePatchers = [
+    () => hijackTimer(),
+    () => hijackWindowListener(),
+    () => hijackHistoryListener(),
+  ];
+
+  const patchersInSandbox = {
+    [SandBoxType.Proxy]: [
+      ...basePatchers,
+      () => patchStrictSandbox(appName, elementGetter, sandbox.sandbox, true, scopedCSS, excludeAssetFilter),
+    ],
+    [SandBoxType.Snapshot]: [
+      ...basePatchers,
+      () => patchLooseSandbox(appName, elementGetter, sandbox.sandbox, true, scopedCSS, excludeAssetFilter),
+    ],
+  };
+
+  return patchersInSandbox[sandbox.type]?.map((patch) => patch());
+}
+
+export function patchAtBootstrapping(
+  appName: string,
+  elementGetter: () => HTMLElement | ShadowRoot,
+  sandbox: ProxySandbox,
+  scopedCSS: boolean,
+  excludeAssetFilter?: Function,
+): Freer[] {
+  const patchersInSandbox = {
+    [SandBoxType.Proxy]: [
+      () => patchStrictSandbox(appName, elementGetter, sandbox.sandbox, false, scopedCSS, excludeAssetFilter),
+    ],
+    [SandBoxType.Snapshot]: [
+      () => patchLooseSandbox(appName, elementGetter, sandbox.sandbox, false, scopedCSS, excludeAssetFilter),
+    ],
+  };
+
+  return patchersInSandbox[sandbox.type]?.map((patch) => patch());
+}
